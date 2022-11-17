dc5f2fdb7e3025798c3e4340666955f3e3662ac9
AMOS <108424978+Ramaunspoken@users.noreply.github.com>
2022-11-17 14:19:53 +0300

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+  let numberArray = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      numberArray.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      numberArray.push('Fizz');
+    } else if (i % 5 === 0) {
+      numberArray.push('Buzz');
+    } else {
+      numberArray.push(i);
+    }
+  }
+  return numberArray;
+};
